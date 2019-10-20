c95fe9f7f31d8750cbd83ede1f30c27165c5be73
Dídac Padilla <didacpadilla20@gmail.com>
2019-10-20 23:52:14 +0200

    Starting to add the new enemy's field of view

---
+++ Assets/PlayerController/Scripts/EnemyStateMachine/FieldOfView.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+
+    [SerializeField] private float viewRadius = 0;
+    [SerializeField] private float viewAngle = 0;
+
+    public Vector3 DirFromAngle(float angleInDegrees)
+    {
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+}
